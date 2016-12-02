8a912a3b50294722cc52a7f0ea4d3df0d154d420
Gabriel Ratcliff <gabriel.ratcliff@accenture.com>
2016-12-02 09:50:35 -0800

    checkout controller updates

--- public/js/controller/cartController.js
+++ public/js/controller/cartController.js
@@ -10,18 +10,25 @@ app.controller('cartController', function (orderService, $location) {
 		console.log(data);
 	});
 
-	// $scope.removeFromCart = function ($index)
+	vm.removeFromCart = function (item){
+		// orderService.removeItem(item, function (data){
+		// 	if (data){
+				vm.shoppingCart.subtotal -= vm.shoppingCart.inventory[vm.shoppingCart.inventory.indexOf(item)].price;
+				vm.shoppingCart.inventory.splice(vm.shoppingCart.inventory.indexOf(item), 1);
+		// 	}
+		// })
+	};
+	
 
 	vm.checkoutCart = function (){
 		var order = {};
 		order.items = vm.shoppingCart.inventory;
 		order.total = vm.shoppingCart.subtotal;
 		order.createdAt = new Date();
-		if (vm.shoppingCart.length > 0){
-			vm.myOrder = order;
-		};
+		vm.myOrder = order;
+
 		vm.shoppingCart = [];
-		// orderService.checkoutCart(order);
+		orderService.checkoutCart(order);
 		// $location.path('/orders');
 	};
 
--- public/js/services/orderService.js
+++ public/js/services/orderService.js
@@ -1,24 +1,21 @@
 app.service('orderService', function ($http, $q){
 
 	this.shoppingCart = {'inventory': [], 'subtotal': 0};
-	var cartReset = this.shoppingCart;
 	this.orders = [];
 
-	// this.getOrders = function (callback){
-	// 	callback()
-	// };
-	
+	this.removeItem = function (item, callback){
+		
+	};
+
 	this.addToCart = function (product){
 		this.shoppingCart.inventory.push(product);
 		this.shoppingCart.subtotal += product.price;
-		console.log(this.shoppingCart);
-
 	};
 
-	// this.checkoutCart = function (cartData){
-	// 	this.orders.push(cartData);
-	// 	this.shoppingCart = cartReset;
-	// };
+	this.checkoutCart = function (cartData){
+		this.orders.push(cartData);
+		this.shoppingCart = {'inventory': [], 'subtotal': 0};
+	};
 
 	this.getShoppingCart = function (callback){
 		callback(this.shoppingCart);
