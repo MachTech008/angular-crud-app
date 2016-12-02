3a39e684b14fa80ba1e75b345381f6e5e86052de
Gabriel Ratcliff <gabriel.ratcliff@accenture.com>
2016-12-02 10:26:32 -0800

    data clean function

--- public/js/controller/productListController.js
+++ public/js/controller/productListController.js
@@ -1,11 +1,25 @@
 app.controller('productListController', function (contentService, orderService) {
 
   var vm = this;
+  var tempObjArray;
 
 	vm.products = [];
 
+  var dataClean = function (data){
+    tempObjArray = [];
+    for (var i = 0; i < data.Items.length; i++){
+      var newObj = {};
+      newObj.name = data.Items[i].name.S;
+      newObj.price = parseInt(data.Items[i].price.N);
+      newObj.description = data.Items[i].description.S;
+      tempObjArray.push(newObj);
+    }
+  }
+
   contentService.getProducts(function (data){
-    vm.products = data;
+      dataClean(data);
+      vm.products = tempObjArray;
+    // vm.products = data;
   });
 
 
