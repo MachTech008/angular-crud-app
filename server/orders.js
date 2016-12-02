753b9b643cfef54d45b430796169f48b1d5092a8
Stanford-Asiyo, Aheri <aheri.stanford-asiyo@accenture.com>
2016-12-02 13:18:35 -0800

    Updated indentation and moved string concat from ES6 to ES5

--- server/orders.js
+++ server/orders.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
 var express = require('express');
 var request = require('request');
 
 var router = express.Router();
-var baseURL = 'http://ec2-54-183-169-236.us-west-1.compute.amazonaws.com:8000'
+var baseURL = 'http://ec2-54-183-169-236.us-west-1.compute.amazonaws.com:8000';
 
 function handleRequest(url, res) {
 
@@ -13,16 +13,16 @@ function handleRequest(url, res) {
       console.error(err);
     }
 
-    res.set('Content-Type', 'application/json');
+    // res.set('Content-Type', 'application/json');
     res.send(body);
   });
 }
 
 router.get('/orders', function(req, res) {
-  var url = `${baseURL}/orders`;
+  var url = baseURL + '/orders';
 
   handleRequest(url, res);
 
-})
+});
 
 module.exports = router;
