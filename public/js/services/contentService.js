ea5deb01169271a39e5f040dbb7d3dce1a4a5149
Stanford-Asiyo, Aheri <aheri.stanford-asiyo@accenture.com>
2016-12-02 13:13:36 -0800

    Updated indentation

--- public/js/services/contentService.js
+++ public/js/services/contentService.js
@@ -2,8 +2,8 @@ app.service('contentService', function ($http, $q){
 
   this.getProducts = function (callback){
     $http.get('/products')
-      .then(function (response){
-        callback(response.data)
+    .then(function (response){
+      callback(response.data);
     }).catch(function (err){
       console.error(err);
     });
