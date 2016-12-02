d9af628ebf0efdb336aca4d29ec59fbac18c0837
Nguyen, Lyly <lyly.nguyen@accenture.com>
2016-12-02 00:13:02 -0800

    included css

--- public/js/services/contentService.js
+++ public/js/services/contentService.js
@@ -1,19 +1,12 @@
 app.service('contentService', function ($http, $q){
 
-  var products = [
-      {'name': 'dogbone', 'price': 16, 'description': 'A succulent chewing toy.'},
-      {'name': 'coffee', 'price': 3, 'description': 'Brown water.'},
-      {'name': 'macbook', 'price': 1400, 'description': 'Useful.'}
-   ];
-
   this.getProducts = function (callback){
-  	// $http.get('https://persist-tasks.herokuapp.com/api/getusers').then(function (data){
-  	// 	console.log(data);
-  	// }).catch(function (err){
-  	// 	console.log(err);
-  	// });
-    callback(products);
+    $http.get('/products')
+      .then(function (response){
+        callback(response.data)
+    }).catch(function (err){
+      console.error(err);
+    });
   };
-
 
 });
